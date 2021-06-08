0cf63c8ff2bb0a17d59dd0fead83bf7b6e78b605
Neamul Islam Fahim <71332807+NeamulIslamFahim@users.noreply.github.com>
2021-06-08 23:27:59 +0600

    Java Loops I

--- Java/Solution.java
+++ Java/Solution.java
@@ -13,22 +13,17 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int sum = 0, i;
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         
-        int n = scanner.nextInt();
-        
-        if(n%2!=0) {
-            System.out.println("Weird");
+        for(i=1; i<=10 ; i++) {
+            
+            sum = N*i;
+            System.out.println(N+" x "+i+" = "+sum);
+           
         }
-        else if(n%2==0 && n>=2 && n<=5) {
-            System.out.println("Not Weird");
-        }
-        
-        else if(n%2==0 && n>=6 && n<=20) {
-            System.out.println("Weird");
-        }
-        
-        else if(n%2==0 && n>20) {
-            System.out.println("Not Weird");
-        }
+
+        scanner.close();
     }
 }
