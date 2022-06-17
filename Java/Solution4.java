fbb7f40e819112db8f559af825a66fadcac11226
Neamul Islam Fahim <71332807+NeamulIslamFahim@users.noreply.github.com>
2022-06-17 22:03:19 +0600

    Java Output Formatting

--- Java/Solution4.java
+++
@@ -1,8 +0,0 @@
-public class Solution4 {
-
-    public static void main(String[] args) {
-        
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
