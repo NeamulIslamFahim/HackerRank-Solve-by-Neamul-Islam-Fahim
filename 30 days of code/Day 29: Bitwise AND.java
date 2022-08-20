b7af588c497946c2576125be880a128800569e0c
Neamul Islam Fahim <71332807+NeamulIslamFahim@users.noreply.github.com>
2022-08-20 10:33:37 +0600

    Day 29: Bitwise AND

---
+++ 30 days of code/Day 29: Bitwise AND.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+     int max = K-1;
+    while(max>0) {            
+        for( int i=N;i>0;i--) {
+            for(int  j=i-1;j>0;j--) {
+                if((i&j) == max ) return max;
+            }
+        }
+        max--;
+    }
+    return 0;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int count = Integer.parseInt(firstMultipleInput[0]);
+
+                int lim = Integer.parseInt(firstMultipleInput[1]);
+
+                int res = Result.bitwiseAnd(count, lim);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
